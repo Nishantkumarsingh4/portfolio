f2116f8cb212bfb67b77b26b4edb086d8b5102f7
nishantkumar018 <nishant.kumar@revcued.com>
2025-11-16 15:59:01 +0530

    final

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Nishant Kumar</span> persuing Bachelor of Technology from Ambalika Institute of Management and Technology. I am a passionate Computer Science student and aspiring Full-Stack Developer with hands-on experience in building scalable and interactive web applications. Currently interning as a Full Stack Developer (Next.js), I love solving real-world problems and continuously improving my development and DSA skills. I enjoy working with modern technologies, exploring new concepts, and contributing to impactful projects."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
